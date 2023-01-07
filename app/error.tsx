ba570894f72f3766f45abf02a84cc5d84504b582
kensiiwasaki <kensiiwasaki@gmail.com>
2023-01-07 22:57:58 +0900

    modify: appDirエラーハンドリング

--- app/error.tsx
+++ app/error.tsx
@@ -3,24 +3,10 @@
 // 'use client' marks this page as a Client Component
 // https://beta.nextjs.org/docs/rendering/server-and-client-components
 
-import { useEffect } from 'react'
-
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error
-  reset: () => void
-}) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
-
+export default function Error() {
   return (
     <div>
-      <p>Something went wrong!</p>
-      <button onClick={() => reset()}>Reset error boundary</button>
+      <p>データの取得に失敗しました</p>
     </div>
   )
 }
