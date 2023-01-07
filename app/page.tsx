382bc855f4a2437cf0365f82f2cd62f44684d583
kensiiwasaki <kensiiwasaki@gmail.com>
2023-01-07 23:03:30 +0900

    modify: 使用するAPIを変更

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import { use } from 'react'
 
 type User = {
   id: number
-  login: string
+  name: string
 }
 
 const fetchUsers: () => Promise<User[]> = async () => {
-  const res = await fetch('https://api.github.com/users')
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
   return res.json()
 }
 
@@ -14,12 +14,12 @@ export default function Home() {
   const users = use(fetchUsers())
   return (
     <div>
-      <h1>GitHub Users!</h1>
+      <h1>jsonplaceholder Users!</h1>
       <div>
         {users.map((user) => {
           return (
             <div key={user.id}>
-              {user.id}: {user.login}
+              {user.id}: {user.name}
             </div>
           )
         })}
